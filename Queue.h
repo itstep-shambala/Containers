e9a20af4213e564c821214a171c72ca83d3b7ff7
AnSt <starininandrey@gmail.com>
2021-02-14 15:39:26 +0300

    Добавление класса Queue

---
+++ Queue.h
@@ -0,0 +1,17 @@
+#ifndef STACK_QUEUE_H
+#define STACK_QUEUE_H
+
+#include <vector>
+#include "Container.h"
+#include "IContainer.h"
+#include "IEnumerable.h"
+
+using namespace std;
+
+template<class T>
+class Queue : public Container<T> {
+
+};
+
+
+#endif //STACK_QUEUE_H
