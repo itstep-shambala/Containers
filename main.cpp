5f8b79ee625799d75ad9fee2dcb3680ffa96310a
GVFedosenko <67539738+GVFedosenko@users.noreply.github.com>
2021-02-21 10:47:05 +0300

    Revert "Gv fedosenko'"

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include "Stack.h"
 #include "utils.h"
-#include "Queue.h"
 
 using namespace std;
 
 int main()
 {
     system("chcp 65001");
-/*
-    Stack<int> temp;
+
+    Stack temp;
 
     Utils::PrintStack(temp);
     temp.Push(5);
@@ -32,26 +31,4 @@ int main()
     temp.ForEach(Utils::Print);
 
     return 0;
-    */
-    Queue<int> temp;
-
-    Utils::PrintQueue(temp);
-    temp.Push(5);
-    Utils::PrintQueue(temp);
-    temp.Push(10);
-    Utils::PrintQueue(temp);
-    int one = temp.Pop();
-    int two = temp.Pop();
-    Utils::PrintQueue(temp);
-    cout << two << " " << one << endl;
-
-    temp.Push(1);
-    temp.Push(2);
-    temp.Push(3);
-    Utils::PrintQueue(temp);
-
-    temp.ForEach(Utils::Print);
-    temp.ForEach(Utils::Plus5);
-    cout << endl;
-    temp.ForEach(Utils::Print);
 }
